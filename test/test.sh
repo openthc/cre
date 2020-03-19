9309792dab1ccb7b088bf8d53f00a9047216481a
openthc <code@openthc.com>
2020-03-19 18:50:33 +0000

    update test config

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -x
 #
-# Test our API
+# OpenTHC Test Runner
 #
 
 set -o errexit
@@ -8,11 +8,10 @@ set -o nounset
 
 f=$(readlink -f "$0")
 d=$(dirname "$f")
-
-cd "$d"
-
 dt=$(date)
 dts=$(date +%Y%m%d-%H%M%S)
+
+cd "$d"
 
 out_path="../webroot/test-output"
 if [ ! -d "$out_path" ]
@@ -23,8 +22,11 @@ fi
 
 #
 #
-../vendor/bin/phpunit "$@" 2>&1 |tee "$out_path/output.txt"
+../vendor/bin/phpunit "$@" 2>&1 | tee "$out_path/output.txt"
+note=$(tail -n1 "$out_path/output.txt")
 
+#
+# Get Transform
 if [ ! -f "phpunit-report.xsl" ]
 then
 	wget https://openthc.com/css/phpunit-report.xsl
@@ -36,39 +38,7 @@ xsltproc \
 	phpunit-report.xsl \
 	"$out_path/output.xml"
 
-note=$(tail -n1 "$out_path/output.txt")
-
-
-cat > "$out_path/index.html" <<HTML
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="initial-scale=1, user-scalable=yes">
-<meta name="theme-color" content="#247420">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
-<link rel="stylesheet" href="https://cdn.openthc.com/bootstrap/4.4.1/bootstrap.css" integrity="sha256-L/W5Wfqfa0sdBNIKN9cG6QA5F2qx4qICmU2VgLruv9Y=" crossorigin="anonymous" />
-<title>Test Result</title>
-</head>
-<body>
-<div class="container">
-<div class="jumbotron mt-4">
-
-<h1>Test Results ${dt}</h1>
-<h2>${note}</h2>
-
-<p>You can view the <a href="output.txt">raw script output</a>,
-or the <a href="output.xml">Unit Test XML</a>
-which we've processed <small>(via XSL)</small> to <a href="output.html">a pretty report</a>
-which is also in <a href="testdox.html">testdox format</a>.
-</p>
-
-<!-- <p>Originally Published at <a href="/test-output-${dts}/">/test-output-${dts}/</a> -->
-
-</div>
-</div>
-</body>
-</html>
-HTML
+sed "s/%TEST_DATE%/$dt/g; s/%TEST_NOTE%/$note/g;" ./report.html > ../webroot/test-output/index.html
 
 #
 # Archive
