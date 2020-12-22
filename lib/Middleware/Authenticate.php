332aadda5a1e6aaa51aa7393498f117ab48e6c2f
openthc <code@openthc.com>
2020-12-22 18:13:28 +0000

    middleware deps

---
+++ lib/Middleware/Authenticate.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Authenticate the Session
+ */
+
+namespace App\Middleware;
+
+class Authenticate extends \OpenTHC\Middleware\Base
+{
+	public function __invoke($REQ, $RES, $NMW)
+	{
+		if (empty($_SESSION['service_id'])) {
+			return $RES->withJSON([ 'meta' => [ 'detail' => 'Not Authorized [LMA-015]' ]], 403);
+		}
+
+		if (empty($_SESSION['company_id'])) {
+			return $RES->withJSON([ 'meta' => [ 'detail' => 'Not Authorized [LMA-022]' ]], 403);
+		}
+
+		if (empty($_SESSION['license_id'])) {
+			return $RES->withJSON([ 'meta' => [ 'detail' => 'Not Authorized [LMA-029]' ]], 403);
+		}
+
+		$RES = $NMW($REQ, $RES);
+
+		return $RES;
+
+	}
+}
---
+++ lib/Middleware/InputDataFilter.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Inflate the JSON inbound
+ */
+
+namespace App\Middleware;
+
+class InputDataFilter extends \OpenTHC\Middleware\Base
+{
+	public function __invoke($REQ, $RES, $NMW)
+	{
+		// Alpha Magic
+		$type = strtolower(strtok($_SERVER['CONTENT_TYPE'], ';'));
+		$size = intval($_SERVER['CONTENT_LENGTH']);
+
+		if (($size > 0) && ('application/json' == $type)) {
+
+			$json = file_get_contents('php://input');
+			$data = json_decode($json, true);
+
+			if (empty($data)) {
+				$e = json_last_error_msg();
+				return $RES->withJSON([
+					'meta' => [ 'detail' => sprintf('JSON Parsing Error "%s" [LMI-025]', $e) ]
+				], 400);
+			}
+
+			$REQ = $REQ->withAttribute('JSON', $data);
+
+			// SHIT HACK overloads $_POST to our JSON object
+			$_POST = $data;
+
+		}
+
+		$RES = $NMW($REQ, $RES);
+
+		return $RES;
+
+	}
+}
