e32b051b5563506ef73889cb8c395d12c788fc13
openthc <code@openthc.com>
2020-12-23 15:46:51 +0000

    fix error codes

--- lib/Middleware/Auth/Simple.php
+++ lib/Middleware/Auth/Simple.php
@@ -9,25 +9,23 @@ class Simple extends \OpenTHC\Middleware\Base
 {
 	public function __invoke($REQ, $RES, $NMW)
 	{
-		session_start();
-
 		$dbc = $this->_container->DB;
 
 		if (empty($_POST['service'])) {
 			return $RES->withJSON([
-				'meta' => [ 'detail' => 'Parameter "service" missing [MAS#017]' ]
+				'meta' => [ 'detail' => 'Parameter "service" missing [MAS-017]' ]
 			], 400);
 		}
 
 		if (empty($_POST['company'])) {
 			return $RES->withJSON([
-				'meta' => [ 'detail' => 'Parameter "company" missing [MAS#025]' ]
+				'meta' => [ 'detail' => 'Parameter "company" missing [MAS-025]' ]
 			], 400);
 		}
 
 		if (empty($_POST['license'])) {
 			return $RES->withJSON([
-				'meta' => [ 'detail' => 'Parameter "license" missing [MAS#033]' ]
+				'meta' => [ 'detail' => 'Parameter "license" missing [MAS-028]' ]
 			], 400);
 		}
 
@@ -38,7 +36,7 @@ class Simple extends \OpenTHC\Middleware\Base
 		$service_id = $dbc->fetchOne($sql, $arg);
 		if (empty($service_id)) {
 			return $RES->withJSON([
-				'meta' => [ 'detail' => 'Invalid "service" [MAS#043]' ]
+				'meta' => [ 'detail' => 'Invalid "service" [MAS-039]' ]
 			], 403);
 		}
 
@@ -49,7 +47,7 @@ class Simple extends \OpenTHC\Middleware\Base
 		$company_id = $dbc->fetchOne($sql, $arg);
 		if (empty($company_id)) {
 			return $RES->withJSON([
-				'meta' => [ 'detail' => 'Invalid "company" [MAS#015]' ]
+				'meta' => [ 'detail' => 'Invalid "company" [MAS-050]' ]
 			], 403);
 		}
 
@@ -60,14 +58,14 @@ class Simple extends \OpenTHC\Middleware\Base
 		$L = $dbc->fetchRow($sql, $arg);
 		if (empty($L['id'])) {
 			return $RES->withJSON([
-				'meta' => [ 'detail' => 'Invalid "license" [MAS#070]' ]
+				'meta' => [ 'detail' => 'Invalid "license" [MAS-061]' ]
 			], 403);
 		}
 
 		// Company and License Match?
 		if ($company_id != $L['company_id']) {
 			return $RES->withJSON([
-				'meta' => [ 'detail' => 'Invalid "license" [MAS#077]' ]
+				'meta' => [ 'detail' => 'Invalid "license" [MAS-068]' ]
 			], 403);
 		}
 
