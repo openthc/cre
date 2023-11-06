8e143c42e523f373d3b8efe4117635e7d26bf0b5
openthc <code@openthc.com>
2023-11-06 15:41:14 +0000

    update example

--- etc/config-example.php
+++ etc/config-example.php
@@ -3,8 +3,10 @@
  * OpenTHC CRE Configuration Example
  */
 
+// Init
 $cfg = [];
 
+// Database
 $cfg['database'] = [
 	'cre' => [
 		'hostname' => '127.0.0.1',
@@ -14,15 +16,17 @@ $cfg['database'] = [
 	]
 ];
 
+// Redis
 $cfg['redis'] = [
 	'hostname' => '127.0.0.1',
 	'database' => '0',
 	'publish' => 'openthc_cre_pub',
 ];
 
+// OpenTHC
 $cfg['openthc'] = [
 	'sso' => [
-		'hostname' => 'sso.openthc.example.com',
+		'origin' => 'https://sso.openthc.example.com',
 	]
 ];
 
