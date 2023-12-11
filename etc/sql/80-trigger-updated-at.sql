b84ee4da00b26a82393d4812ac7827041b45acb6
openthc <code@openthc.com>
2023-12-11 04:08:17 +0000

    fix WS

--- etc/sql/80-trigger-updated-at.sql
+++ etc/sql/80-trigger-updated-at.sql
@@ -29,105 +29,105 @@ CREATE TRIGGER set_updated_at_company
 
 
 CREATE TRIGGER set_updated_at_license
-        BEFORE UPDATE
-        ON license
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON license
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_section
-        BEFORE UPDATE
-        ON section
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON section
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_variety
-        BEFORE UPDATE
-        ON variety
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON variety
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_product
-        BEFORE UPDATE
-        ON product
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON product
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_vehicle
-        BEFORE UPDATE
-        ON vehicle
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON vehicle
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_crop
-        BEFORE UPDATE
-        ON crop
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON crop
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_crop_collect
-        BEFORE UPDATE
-        ON crop_collect
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON crop_collect
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_inventory
-        BEFORE UPDATE
-        ON inventory
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON inventory
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_lab_result
-        BEFORE UPDATE
-        ON lab_result
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON lab_result
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_b2b_incoming
-        BEFORE UPDATE
-        ON b2b_incoming
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON b2b_incoming
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_b2b_incoming_item
-        BEFORE UPDATE
-        ON b2b_incoming_item
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON b2b_incoming_item
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_b2b_outgoing
-        BEFORE UPDATE
-        ON b2b_outgoing
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON b2b_outgoing
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_b2b_outgoing_item
-        BEFORE UPDATE
-        ON b2b_outgoing_item
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON b2b_outgoing_item
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_b2c_sale
-        BEFORE UPDATE
-        ON b2c_sale
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON b2c_sale
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
 
 
 CREATE TRIGGER set_updated_at_b2c_sale_item
-        BEFORE UPDATE
-        ON b2c_sale_item
-        FOR EACH ROW
-        EXECUTE PROCEDURE set_updated_at_trigger();
+	BEFORE UPDATE
+	ON b2c_sale_item
+	FOR EACH ROW
+	EXECUTE PROCEDURE set_updated_at_trigger();
