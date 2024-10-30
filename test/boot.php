3f634144605f4cd59a6b697d8d64376f96728167
openthc <code@openthc.com>
2024-10-30 18:13:12 +0000

    use php based test runner

--- test/boot.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * PHPUnit Test Bootstrap file
- * See composer.json in project source root for dependencies.
- */
-
-// Load App bootstrap file
-require_once(dirname(dirname(__FILE__)) . '/boot.php');
